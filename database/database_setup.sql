08d6165aa2b3757bc4c53504f0abf40e5e8dc55f
Violetukun <136240943+Violetukun@users.noreply.github.com>
2026-03-21 10:46:14 +0800

    Update database_setup.sql

--- database/database_setup.sql
+++ database/database_setup.sql
@@ -9,8 +9,6 @@ CREATE TABLE patient_results (
     result_pdf_key VARCHAR(255)
 );
 
--- Insert a test patient so we can try logging in
-INSERT INTO patient_results (reference_number, last_name, result_status) 
-VALUES ('REF12345', 'DELAPENA', 'Available');
-
-UPDATE patients SET result_pdf_key = 'results/REF12345.pdf' WHERE reference_number = 'REF12345';
+-- Note: 'REF12345.pdf' assumes you uploaded the file directly to the main S3 bucket folder.
+INSERT INTO patient_results (reference_number, last_name, result_status, result_pdf_key) 
+VALUES ('REF12345', 'DELAPENA', 'Available', 'REF12345.pdf');
